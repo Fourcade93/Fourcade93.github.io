d496bbf28471354bf32e1b8e7fa84921953af576
madina <madina@ArtBook.local>
2017-09-12 20:46:54 +0300

    form

--- form.php
+++ form.php
@@ -1,33 +1,26 @@
-<!doctype html>
-<html>
-<head>
-   <meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
-   <title>Ваше сообщение успешно отправлено</title>
-</head>
- 
-<body>
- 
 <?php
-   $back = "<p><a href=\"javascript: history.back()\">Вернуться назад</a></p>";
- 
-   if(!empty($_POST['nm']) and !empty($_POST['nmb'])){
-      $name = trim(strip_tags($_POST['nm']));
-      $phone = trim(strip_tags($_POST['nmb']));
-      $message = trim(strip_tags($_POST['msg']));
- 
-      mail('bomcrimea@gmail.com', 'Письмо с RoomAurum', 
-      'Вам написал: '.$name.'<br />Его номер: '.$phone.'<br />
-      Его сообщение: '.$message,"Content-type:text/html;charset=windows-1251");
- 
-      echo "Ваше сообщение успешно отправлено!<Br> Вы получите ответ в 
-      ближайшее время<Br> $back";
- 
-      exit;
-   } 
-   else {
-      echo "Для отправки сообщения заполните все поля! $back";
-      exit;
-   }
+/* Осуществляем проверку вводимых данных и их защиту от враждебных 
+скриптов */
+$name = htmlspecialchars($_POST["name"]);
+$phone = htmlspecialchars($_POST["phone"]);
+$message = htmlspecialchars($_POST["message"]);
+/* Устанавливаем e-mail адресата */
+$myemail = "bomcrimea@gmail.ru";
+/* Проверяем заполнены ли обязательные поля ввода, используя check_input 
+функцию */
+$name = check_input($_POST["name"], "Введите ваше имя!");
+$phone = check_input($_POST["phone"], "Введите номер телефона!");
+
+/* Создаем новую переменную, присвоив ей значение */
+$message_to_myemail = "Здравствуйте! 
+Вашей контактной формой было отправлено сообщение! 
+Имя отправителя: $name 
+Телефон: $phone 
+Текст сообщения: $message 
+Конец";
+/* Отправляем сообщение, используя mail() функцию */
+$from  = "From: $name <$phone> \r\n Reply-To: $phone \r\n"; 
+mail($myemail, $name, $message_to_myemail, $from);
 ?>
-</body>
-</html>
+<p>Ваше сообщение было успешно отправлено!</p>
+<p>На <a href="index.html">Главную >>></a></p>
